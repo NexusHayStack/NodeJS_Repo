380d8f545e84aa84af1941b3afae80ac6e846012
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-06-16 10:55:10 +0530

    Background Workers (server.js): Refactored the whole index.js code to a new file in /lib/ dir. named server.js and edited all the local-file variable into object variable that is exported inside the server {} object + Deleted the @TODO line that was originally in index.js.

--- My NodeJS DIR/Section 3/my-app/index.js
+++ My NodeJS DIR/Section 3/my-app/index.js
@@ -4,125 +4,24 @@
  */
 
 //Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var config = require('./lib/config');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers')
-var fs = require('fs');
-var StringDecoder = require('string_decoder').StringDecoder;
-/* string_decoder is a vast lib. we need 			^
- * a fraction of that and string_decoder			|
- * parameter contains the functions we need ---------
- */
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
-// @TODO GET RID OF THIS
-helpers.sendTwilioSms('7845934678','Hello!',function(err){
-	console.log('this was the error',err);
-});
+// Declare the app 
+var app = {};
 
-// Instantiate the HTTP server
-var httpServer = http.createServer(function(req,res){
-	unifiedServer(req,res);
-});
+// Init function
+app.init = function(){
+	// Start the server
+	server.init();
 
-
-// Start the server
-httpServer.listen(config.httpPort,function(){
-	console.log("The Server is now listening on port " +config.httpPort+ " in " +config.envName+ " mode");
-});
-
-// Instantiate the HTTPS server
-var httpsServerOptions = {
-	'key' : fs.readFileSync('./https/key.pem'),
-	'cert' : fs.readFileSync('./https/cert.pem')
-};
-var httpsServer = https.createServer(httpsServerOptions,function(req,res){
-	unifiedServer(req,res);
-});
-
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort,function(){
-	console.log("The Server is now listening on port " +config.httpsPort+ " in " +config.envName+ " mode");
-});
-
-// All the server logic for both the http and https createServer
-var unifiedServer = function(req,res){
-	
-	// Get the URL and parse it
-	var parsedUrl = url.parse(req.url,true);
-
-	// Get the path
-	var path = parsedUrl.pathname;
-	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-	// Get the string query as an Object
-	var queryStringObject = parsedUrl.query;
-
-	// Get the HTTP Method
-	var method = req.method.toLowerCase();
-
-	// Get the headers as an object 
-	var headers = req.headers;
-
-	// Get the payload, if any
-	var decoder = new StringDecoder('utf-8')		/* decoder object to decode a utf-8 to a string */
-	var buffer = '';							    
-	req.on('data', function(data){					/* takes the 'data', when 'data' event is emitted by the 'req' object, and plug it in a call back function */
-		buffer += decoder.write(data);			    /* appending the stream of decoded 'data' to buffer "bit-by-bit"*/
-	});
-	
-	// Stop the binding on the end of the stream
-	req.on('end', function(){						/* This function stops appending the string when the 'end' event of the stream is detected, i.e. it will be executed weather there is a payload or not*/
-		buffer += decoder.end();
-
-		// Choose the handler this request should go to. If one is not found, use the notFound handler.
-		var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-		// Construct the data object to send to the handler
-		var data = {
-			'trimmedPath' : trimmedPath,
-			'queryStringObject' : queryStringObject,
-			'method' : method,
-			'headers' : headers,
-			'payload' : helpers.parseJsonToObject(buffer)
-		};
-		
-		// Route the request to the handler specified in the router
-		chosenHandler(data,function(statusCode,payload){
-			//Use the status code called back by the handler, or default to 200
-			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-			// Use the payload called back by the handler, or default to an empty object
-			payload = typeof(payload) == 'object' ? payload : {};
-			
-
-			//Convert the payload to a string
-			var payloadString = JSON.stringify(payload);
-
-			// Send the response
-				// Sending a 'Header Content' = json, telling the client browser that server is sending a json type
-			res.setHeader('Content-Type','application/json');
-			res.writeHead(statusCode);
-			res.end(payloadString);
-
-			// Log the requested payload
-			console.log(trimmedPath,statusCode);
-		});
-
-		
-	});
-
-	
+	// Start the workers
+	workers.init();
 };
 
+// Execute
+app.init();
 
 
-// Define a request router
-var router = {
-	'ping' : handlers.ping,
-	'users' : handlers.users,
-	'tokens' : handlers.tokens,
-	'checks' : handlers.checks
-}
+// Export the app 
+module.exports = app;

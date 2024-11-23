d46b668092d804a95ca5ae73436494e10ba6c108
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-11-23 14:37:33 +0530

    Page 5 - Edit Account: Added templates/accountEdit.html with handlers.accountEdit() to serve the 'Account Setting' page. Separated processing logic by looping through all <form> data in app.bindForms(). Integrated app.loadDataOnPage() into the app initialization process, calling app.loadAccountEditPage() to display user info (excluding password). Updated app.css to hide 'formError' and 'formSuccess' by default, dynamically handled in app.js.

--- My NodeJS DIR/Section 4/my-app/lib/handlers.js
+++ My NodeJS DIR/Section 4/my-app/lib/handlers.js
@@ -120,7 +120,7 @@ handlers.sessionCreate = function(data,callback){
 
 // Session has been deleted
 handlers.sessionDeleted = function(data,callback){
-		// Reject any request that isn't a GET
+	// Reject any request that isn't a GET
 	if(data.method == 'get'){
 
 		// Prepare data for interpolation
@@ -133,6 +133,39 @@ handlers.sessionDeleted = function(data,callback){
 
 		// Read in a template as a string
 		helpers.getTemplate('sessionDeleted',templateData,function(err,str){
+			if(!err && str){
+				// Add the universal header and footer
+				helpers.addUniversalTemplates(str,templateData,function(err,str){
+					if(!err && str){
+						// Return that page as HTML
+						callback(200,str,'html');
+					} else {
+						callback(500,undefined,'html')
+					}
+				});
+			} else {
+				callback(500,undefined,'html');
+			}
+		});
+	} else {
+		callback(405,undefined,'html');
+	}
+};
+
+// Edit your account
+handlers.accountEdit = function(data,callback){
+	// Reject any request that isn't a GET
+	if(data.method == 'get'){
+
+		// Prepare data for interpolation
+		var templateData = {
+			'head.title' : 'Account Settings',
+			'body.class' : 'accountEdit'
+		};
+
+
+		// Read in a template as a string
+		helpers.getTemplate('accountEdit',templateData,function(err,str){
 			if(!err && str){
 				// Add the universal header and footer
 				helpers.addUniversalTemplates(str,templateData,function(err,str){
--- My NodeJS DIR/Section 4/my-app/public/app.js
+++ My NodeJS DIR/Section 4/my-app/public/app.js
@@ -16,13 +16,13 @@ app.config = {
 app.client = {};
 
 // Interface for making API calls 
-app.client.request = function(headers,path,method,querStringObject,payload,callback){
+app.client.request = function(headers,path,method,queryStringObject,payload,callback){
 
 	// Set defaults
 	headers = typeof(headers) == 'object' && headers !== null ? headers : {};
 	path = typeof(path) == 'string' ? path : '';
 	method = typeof(method) == 'string' && ['POST','GET','PUT','DELETE'].indexOf(method.toUpperCase()) > -1 ? method.toUpperCase() : 'GET';
-	queryStringObject = typeof(querStringObject) == 'object' && querStringObject !== null ? querStringObject : {};
+	queryStringObject = typeof(queryStringObject) == 'object' && queryStringObject !== null ? queryStringObject : {};
 	payload = typeof(payload) == 'object' && payload !== null ? payload : {};
 	callback = typeof(callback) == 'function' ? callback : false;
 
@@ -37,7 +37,7 @@ app.client.request = function(headers,path,method,querStringObject,payload,callb
 				requestUrl+='&';
 			}
 			// Add the key and value
-			requestUrl+=queryKey+'='+querStringObject[queryKey];
+			requestUrl+=queryKey+'='+queryStringObject[queryKey];
 		}
 	}
 
@@ -118,54 +118,68 @@ app.logUserOut = function(){
 // Bind the forms
 app.bindForms = function(){
 	if(document.querySelector("form")){
-	  document.querySelector("form").addEventListener("submit", function(e){
+	  var allForms = document.querySelectorAll("form");
+    for(var i = 0; i < allForms.length; i++){
+      allForms[i].addEventListener("submit", function(e){
 
-	    // Stop it from submitting
-	    e.preventDefault();
-	    var formId = this.id;
-	    var path = this.action;
-	    var method = this.method.toUpperCase();
+      // Stop it from submitting
+      e.preventDefault();
+      var formId = this.id;
+      var path = this.action;
+      var method = this.method.toUpperCase();
 
-	    // Hide the error message (if it's currently shown due to a previous error)
-	    document.querySelector("#"+formId+" .formError").style.display = 'hidden';
+      // Hide the error message (if it's currently shown due to a previous error)
+      document.querySelector("#"+formId+" .formError").style.display = 'none';
 
-	    // Turn the inputs into a payload
-	    var payload = {};
-	    var elements = this.elements;
-	    for(var i = 0; i < elements.length; i++){
-	      if(elements[i].type !== 'submit'){
-	        var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
-	        payload[elements[i].name] = valueOfElement;
-	      }
-	    }
+      // Hide the success message (if it's currently shown due to a previous error)
+      if(document.querySelector("#"+formId+" .formSuccess")){
+        document.querySelector("#"+formId+" .formSuccess").style.display = 'none';
+      }
 
-	    // Call the API
-	    app.client.request(undefined,path,method,undefined,payload,function(statusCode,responsePayload){
-        // Display an error on the form if needed
-        if(statusCode !== 200){
 
-        	if(statusCode == 403){
-	          // log the user out
-	          app.logUserOut();
-            
+      // Turn the inputs into a payload
+      var payload = {};
+      var elements = this.elements;
+      for(var i = 0; i < elements.length; i++){
+        if(elements[i].type !== 'submit'){
+          var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
+          if(elements[i].name == '_method'){
+            method = valueOfElement;
           } else {
+            payload[elements[i].name] = valueOfElement;
+          }
 
-	          // Try to get the error from the api, or set a default error message
-	          var error = typeof(responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
-
-	          // Set the formError field with the error text
-	          document.querySelector("#"+formId+" .formError").innerHTML = error;
-
-	          // Show (unhide) the form error field on the form
-	          document.querySelector("#"+formId+" .formError").style.display = 'block';
-	        }
-        } else {
-          // If successful, send to form response processor
-          app.formResponseProcessor(formId,payload,responsePayload);
+          }
         }
 
-	    });
-	  });
+        // Call the API
+        app.client.request(undefined,path,method,undefined,payload,function(statusCode,responsePayload){
+          // Display an error on the form if needed
+          if(statusCode !== 200){
+
+            if(statusCode == 403){
+              // log the user out
+              app.logUserOut();
+
+            } else {
+
+              // Try to get the error from the api, or set a default error message
+              var error = typeof(responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
+
+              // Set the formError field with the error text
+              document.querySelector("#"+formId+" .formError").innerHTML = error;
+
+              // Show (unhide) the form error field on the form
+              document.querySelector("#"+formId+" .formError").style.display = 'block';
+            }
+          } else {
+            // If successful, send to form response processor
+            app.formResponseProcessor(formId,payload,responsePayload);
+          }
+
+        });
+      });
+    }
 	}
 };
 
@@ -195,12 +209,18 @@ app.formResponseProcessor = function(formId,requestPayload,responsePayload){
         app.setSessionToken(newResponsePayload);
         window.location = '/checks/all';
       }
-	});
+		});
   }
   // If login was successful, set the token in localstorage and redirect the user
   if(formId == 'sessionCreate'){
     app.setSessionToken(responsePayload);
     window.location = '/checks/all';
+  }
+
+  // If forms saved successfully and they have success messages, show them
+  var formsWithSuccessMessages = ['accountEdit1', 'accountEdit2'];
+  if(formsWithSuccessMessages.indexOf(formId) > -1){
+    document.querySelector("#"+formId+" .formSuccess").style.display = 'block';
   }
 };
 
@@ -280,6 +300,53 @@ app.renewToken = function(callback){
   }
 };
 
+// Load data on the page
+app.loadDataOnPage = function(){
+  // Get the current page from the body class
+  var bodyClasses = document.querySelector("body").classList;
+  var primaryClass = typeof(bodyClasses[0]) == 'string' ? bodyClasses[0] : false;
+
+  // Logic for account settings page
+  if(primaryClass == 'accountEdit'){
+    app.loadAccountEditPage();
+  }
+};
+
+// Load the account edit page specifically
+app.loadAccountEditPage = function(){
+  // Get the phone number from the current token, or log the user out if none is there
+  var phone = typeof(app.config.sessionToken.phone) == 'string' ? app.config.sessionToken.phone : false;
+  if(phone){
+    // Fetch the user data
+    var queryStringObject = {
+      'phone' : phone
+    };
+    app.client.request(undefined,'api/users','GET',queryStringObject,undefined,function(statusCode,responsePayload){
+      if(statusCode == 200){
+        // Put the data into the forms as values where needed
+        document.querySelector("#accountEdit1 .firstNameInput").value = responsePayload.firstName;
+        document.querySelector("#accountEdit1 .lastNameInput").value = responsePayload.lastName;
+        document.querySelector("#accountEdit1 .displayPhoneInput").value = responsePayload.phone;
+
+        // Put the hidden phone field into both forms
+        var hiddenPhoneInputs = document.querySelectorAll("input.hiddenPhoneNumberInput");
+        for(var i = 0; i < hiddenPhoneInputs.length; i++){
+            hiddenPhoneInputs[i].value = responsePayload.phone;
+        }
+
+      } else {
+        // If the request comes back as something other than 200, log the user out (on the assumption that the api is temporarily down or the users token is bad)
+        app.logUserOut();
+      }
+    });
+  } else {
+    app.logUserOut();
+  }
+
+
+
+};
+
 // Loop to renew token often
 app.tokenRenewalLoop = function(){
   setInterval(function(){
@@ -290,13 +357,6 @@ app.tokenRenewalLoop = function(){
     });
   },1000 * 60);
 };
-
-
-
-
-
-
-
 
 // Init (bootstrapping)
 app.init = function(){
@@ -312,6 +372,10 @@ app.init = function(){
 
   // Renew token
   app.tokenRenewalLoop();
+
+  // Load data on page
+  app.loadDataOnPage();
+
 };
 
 // Call the init processes after the window loads
